aa32d88f7a3e48a99ced85823dc9c3fdbd2c9113
Huy Pham <huy.pham@objectbrains.com>
2019-01-15 16:51:37 -0800

    Huy: Add internationalization.

--- src/main/java/com/phamhuy/example1/Example1Application.java
+++ src/main/java/com/phamhuy/example1/Example1Application.java
@@ -1,13 +1,26 @@
 package com.phamhuy.example1;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class Example1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Example1Application.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver =  new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
 	}
 
 }
--- src/main/java/com/phamhuy/example1/user/UserController.java
+++ src/main/java/com/phamhuy/example1/user/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@Autowired
 	private UserService userService;
@@ -35,5 +40,10 @@ public class UserController {
 	@PostMapping(path = "/users")
 	public long createUser(@Valid @RequestBody User user) {
 		return userService.createUser(user);
+	}
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
